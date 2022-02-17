83f8220236a89ffb624960a31665eaee6ff61e65
HiteshSuryawanshi-24 <hitesh291998@gmail.com>
2022-02-17 14:31:01 +0530

    ADDED: Addition of Two Number

---
+++ ADDITION.C
@@ -0,0 +1,12 @@
+//Program 1.Addition of 2 Numbers
+#include<stdio.h>      //Header Files
+#include<conio.h>
+void main()
+{
+    int a=10,b=20,c=0;
+    clrscr();
+    printf("\nWelcome to Program of Addition of 2 Numbers..Developed by Hitesh!!");
+    c=a+b;
+    printf("\nAddition: %d",c);
+    getch();
+}
